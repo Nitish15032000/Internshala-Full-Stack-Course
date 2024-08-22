3ee0fe79b151184ff17eb14aed10d9ab1532dcf7
Nitish kumar <nitish.edu.in@gmail.com>
2024-08-22 11:02:19 +0530

    Initial commit

---
+++ Java Script/Basic/index.js
@@ -0,0 +1,9 @@
+console.log("shree ganeshay \n namhh");
+// console.log(5||3);
+// console.log(7||0);
+// console.log(0||3);
+// console.log(0||0);
+console.log(5 && 3);
+console.log(7 && 0);
+console.log(0 && 3);
+console.log(0 && 0);
---
+++ Java Script/Basic/truthyValue.js
@@ -0,0 +1,5 @@
+console.log(!(0))
+console.log(!(-0))
+console.log(!(+0))
+console.log(!(+4))
+console.log(!{}) 
