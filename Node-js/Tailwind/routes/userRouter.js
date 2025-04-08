e9d1c6c87b9f783caf186102961c997a1f0d739e
Nitish kumar <nitish.edu.in@gmail.com>
2025-04-08 15:01:43 +0530

    some changes

--- Node-js/Tailwind/routes/userRouter.js
+++ Node-js/Tailwind/routes/userRouter.js
@@ -1,1 +1,17 @@
-const 
+// Core Modules
+const path = require('path');
+
+// External Module
+const express = require('express');
+const userRouter = express.Router();
+
+// Local Module
+const rootDir = require("../util/PathUtil");
+
+// userRouter.use(express.static(path.join(rootDir, 'public')));
+
+userRouter.get("/contact", (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'view', 'contact.html'));
+});
+
+module.exports = userRouter;
--- Node-js/Tailwind/server.js
+++ Node-js/Tailwind/server.js
@@ -5,20 +5,29 @@ const path = require('path');
 const express = require('express');
 
 // Local Module
-const userRouter = require('./routes/userRouter');
-const rootdir = require('./util/util');
+const rootdir = require('./util/PathUtil');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log(req.method, req.url);
-  next();
-});
+app.use(express.static(path.join(rootdir, 'public')));
+
+
+
+// app.use((req, res, next) => {
+//   console.log(req.method, req.url);
+//   next();
+// });
+
+// router is used for routing the request to different pages
+const userRouter = require("./routes/userRouter")
+app.use(userRouter);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(rootdir, 'view', 'Home.html'));
 });
 
+
+// used for creating a server
 const PORT = 3005;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
